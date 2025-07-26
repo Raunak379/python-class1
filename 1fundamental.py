2480e8c9f58ba77bab59076ed84adf6ca9a33f02
Raunak Kumar Singh <rs114408@gmail.com>
2025-07-26 13:16:21 +0530

    Add files via upload

---
+++ 1fundamental.py
@@ -0,0 +1,18 @@
+
+#print("raunak is my name")
+
+#num1 = 10
+#num2 = 20
+# sum =  num1 + num2
+# print(sum)
+
+# multiply = num1 * num2
+# print(multiply)
+
+
+name = "mohit"
+print(name)
+
+num = 5
+nums = 2
+print(num+nums)
